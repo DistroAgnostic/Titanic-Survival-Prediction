007563fa410ea29ec307d9db98a6c240c1bb29cb
CHETAN PATEL <98891600+DistroAgnostic@users.noreply.github.com>
2025-09-09 14:40:04 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,40 +1,44 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, Field, computed_field
-from typing import Annotated,Literal
+from pydantic import BaseModel, Field
+from typing import Literal
 import pickle
 import pandas as pd
 
-with open("titanic_model.pkl", "rb") as f:
+with open("log_model.pkl", "rb") as f:   
     model = pickle.load(f)
 
-with open("label_encoders.pkl", "rb") as f:
-    encoders = pickle.load(f)
+with open("sex_encoder.pkl", "rb") as f:
+    sex_enc = pickle.load(f)
+
+with open("emb_encoder.pkl", "rb") as f:
+    emb_enc = pickle.load(f)
 
 app = FastAPI(title="Titanic Survival Prediction API")
 
 class Passenger(BaseModel):
-    Pclass: Annotated[int, Field(...,gt = 1, ls = 4, description="Passanger Class")]
-    Sex: Annotated[Literal['male','female'], Field(..., description="Gender of the user")]  # 'male' or 'female'
-    Age: Annotated[int, Field(..., gt = 0, lt = 120,description="Age of the Passenger")]
-    SibSp: Annotated[int, Field(..., gt = 0, lt = 120,description="SibSp of the user")]
-    Parch: Annotated[float, Field(...,description="Parch of the passenger")]
-    Fare: Annotated[int, Field(...,description="Fare of the Passenger")]
-    Embarked: Annotated[Literal["S","C","Q"], Field(..., description="Age of the user")]
+    Pclass:   int   = Field(..., ge=1, le=3)               
+    sex:      Literal['male', 'female']                    
+    Age:      int   = Field(..., ge=0,  le=120)
+    SibSp:    int   = Field(..., ge=0,  le=120)
+    Parch:    int   = Field(..., ge=0,  le=120)            
+    Fare:     float = Field(..., ge=0.0)                   
+    Embarked: Literal['S', 'C', 'Q']                       
 
 @app.post("/predict")
 def predict_survival(passenger: Passenger):
-    sex_encoded = encoders['Sex'].transform([passenger.Sex])[0]
-    embarked_encoded = encoders['Embarked'].transform([passenger.Embarked])[0]
+    sex_enc_val = sex_enc.transform([passenger.sex])[0]
+    emb_enc_val = emb_enc.transform([passenger.Embarked])[0]
 
     input_df = pd.DataFrame([{
-        "Pclass": passenger.Pclass,
-        "Sex": sex_encoded,
-        "Age": passenger.Age,
-        "SibSp": passenger.SibSp,
-        "Parch": passenger.Parch,
-        "Fare": passenger.Fare,
-        "Embarked": embarked_encoded
+        "Pclass":   passenger.Pclass,
+        "Sex":      sex_enc_val,
+        "Age":      passenger.Age,
+        "SibSp":    passenger.SibSp,
+        "Parch":    passenger.Parch,
+        "Fare":     passenger.Fare,
+        "Embarked": emb_enc_val
     }])
 
-    prediction = model.predict(input_df)[0]
-    return {"survived": int(prediction)}
+    pred  = int(model.predict(input_df)[0])
+    proba = float(model.predict_proba(input_df)[0, pred])
+    return {"survived": pred, "probability": proba}
