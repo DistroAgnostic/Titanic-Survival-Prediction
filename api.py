605b1753e4296d7ea77c17ed7171d59bf813ef25
CHETAN PATEL <98891600+DistroAgnostic@users.noreply.github.com>
2025-09-09 12:35:53 +0530

    Create api.py

---
+++ api.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field, computed_field
+from typing import Annotated,Literal
+import pickle
+import pandas as pd
+
+with open("titanic_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+with open("label_encoders.pkl", "rb") as f:
+    encoders = pickle.load(f)
+
+app = FastAPI(title="Titanic Survival Prediction API")
+
+class Passenger(BaseModel):
+    Pclass: Annotated[int, Field(...,gt = 1, ls = 4, description="Passanger Class")]
+    Sex: Annotated[Literal['male','female'], Field(..., description="Gender of the user")]  # 'male' or 'female'
+    Age: Annotated[int, Field(..., gt = 0, lt = 120,description="Age of the Passenger")]
+    SibSp: Annotated[int, Field(..., gt = 0, lt = 120,description="SibSp of the user")]
+    Parch: Annotated[float, Field(...,description="Parch of the passenger")]
+    Fare: Annotated[int, Field(...,description="Fare of the Passenger")]
+    Embarked: Annotated[Literal["S","C","Q"], Field(..., description="Age of the user")]
+
+@app.post("/predict")
+def predict_survival(passenger: Passenger):
+    sex_encoded = encoders['Sex'].transform([passenger.Sex])[0]
+    embarked_encoded = encoders['Embarked'].transform([passenger.Embarked])[0]
+
+    input_df = pd.DataFrame([{
+        "Pclass": passenger.Pclass,
+        "Sex": sex_encoded,
+        "Age": passenger.Age,
+        "SibSp": passenger.SibSp,
+        "Parch": passenger.Parch,
+        "Fare": passenger.Fare,
+        "Embarked": embarked_encoded
+    }])
+
+    prediction = model.predict(input_df)[0]
+    return {"survived": int(prediction)}
